ea1faa7f563369c67981b9eef30cb75df13cfe7d
Dohyuk You <okdohyuk@gmail.com>
2025-08-13 06:22:12 +0900

    feat: add icon and grab cursor to rotator

--- web/libs/editor/src/tags/control/PolygonLabels.jsx
+++ web/libs/editor/src/tags/control/PolygonLabels.jsx
@@ -35,10 +35,11 @@ import ControlBase from "./Base";
  * @param {string} [fillColor]                      - Polygon fill color in hexadecimal
  * @param {string} [strokeColor]                    - Stroke color in hexadecimal
  * @param {number} [strokeWidth=1]                  - Width of stroke
+ * @param {boolean} [canRotate=true]                - Show or hide rotation control
  * @param {small|medium|large} [pointSize=medium]   - Size of polygon handle points
  * @param {rectangle|circle} [pointStyle=rectangle] - Style of points
  * @param {pixel|none} [snap=none]                  - Snap polygon to image pixels
- */
+*/
 
 const Validation = types.model({
   controlledTags: Types.unionTag(["Image"]),
